c26f9a6c089747623a955bbb42a575c01c1de7ed
james94 <jamesmedel94@gmail.com>
2020-12-12 19:10:22 -0600

    CAdded a Check for verifying input matches hidden word
    - This check was added to OnInput(...) function. This function is triggered each
    time the user presses enter on the Terminal

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,16 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to the Bull Cows Game"));
+    PrintLine(TEXT("Guess the word")); // Length of characters not specified
     PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("uncopyrightable");
-    PrintLine(Input);
+    FString HiddenWord = TEXT("uncopyrightable"); // Move outside this function
+    if(Input == HiddenWord)
+        PrintLine(TEXT("You have Won!"));
+    else
+        PrintLine(TEXT("You have Lost!"));
 }
