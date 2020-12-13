bb308572f98825c04b8ed552181ba06ed5fd01f9
james94 <jamesmedel94@gmail.com>
2020-12-12 19:35:04 -0600

    Elevated HiddenWord variable to be a class member variable
    - Since HiddenWord variable is a class member variable, it is accessible throughout the class

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -7,12 +7,13 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("Welcome to the Bull Cows Game"));
     PrintLine(TEXT("Guess the word")); // Length of characters not specified
     PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("uncopyrightable");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("uncopyrightable"); // Move outside this function
+
     if(Input == HiddenWord)
         PrintLine(TEXT("You have Won!"));
     else
