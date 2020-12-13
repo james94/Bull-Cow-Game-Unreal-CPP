4a98922b500ed7e98add933a3c6e117be656c8fc
james94 <jamesmedel94@gmail.com>
2020-12-13 12:38:49 -0600

    Added Pseudocode Comments based on Flow Chart v2

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,16 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Welcoming The Player
     PrintLine(TEXT("Welcome to the Bull Cows Game"));
     PrintLine(TEXT("Guess the word")); // Length of characters not specified
     PrintLine(TEXT("Press enter to continue..."));
+
+    // Setting Up Game
     HiddenWord = TEXT("uncopyrightable");
+    // Set Lives
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -18,4 +24,20 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
         PrintLine(TEXT("You have Won!"));
     else
         PrintLine(TEXT("You have Lost!"));
+
+    // Check If Isogram
+    // Prompt To Guess Again
+
+    // Check If It Is The Right Amount of Characters
+    // Prompt To Guess Again
+
+    // Decrement 1 Life
+
+    // Check If Lives > 0
+    // If Yes, Guess Again
+    // Show Lives Left
+    // If No, Show Game over and Hidden Word?
+    // Prompt To Play Again, Press Enter To Play Again?
+    // Check User Input
+    // Play Again Or Quit
 }
